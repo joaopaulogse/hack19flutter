8310de296b42968bd4cbc82c2fccc4627446dc4e
Jamessom Queiroz <jamessomqueiroz@gmail.com>
2019-06-01 14:33:35 -0300

    add functionallity to home screen

--- lib/pages/my_home_page.dart
+++ lib/pages/my_home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hack19flutter/pages/chat_list.dart';
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -10,43 +11,41 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final _formKey = GlobalKey<FormState>();
+  ScrollController _scroll = new ScrollController();
+
+  var name = '';
+
+  void dispose(){
+    _scroll.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomPadding: false,
-      // body: SafeArea(
-      //   child: Container(
-      //     margin: EdgeInsets.all(20.0),
-      //     child: Form(
-      //       key: _formKey,
-      //       child: Column(
-      //         children: [
-      //           Container(margin: EdgeInsets.only(top: 80.0),),
-      //           imageScreen(),
-      //           nameInput(),
-      //           // buttonActions(),
-      //         ],
-      //       ),
-      //     ),
-      //   ),
-      // ),
       body: _body(),
     );
   }
 
   _body() {
-    return Center(
-      child: SingleChildScrollView(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            //Container(margin: EdgeInsets.only(top: 80.0),),
-            imageScreen(),
-            nameInput(),
-          ],
-        ),
-      ),
+    return SafeArea(
+      child: Container(
+        margin: EdgeInsets.all(20.0),
+        child: Form(
+          key: _formKey,
+          child: Center(
+            child: ListView(
+              controller: _scroll,
+              children: [
+                imageScreen(),
+                nameInput(),
+                buttonActions(),
+              ],
+            ),
+          ),
+        )
+      )
     );
   }
 
@@ -58,33 +57,42 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       validator: (value) {
         if (value.isEmpty) {
-          return 'Name canot be empty';
+          return "Name can't be empty";
         }
       },
+      onSaved: (String value) => name = value
     );
   }
 
   Widget imageScreen(){
-    return Image.asset('images/flutter.png');
+    return Image.asset(
+      'images/flutter.png',
+      height: 120.0,
+      width: 120.0,
+    );
   }
 
-  // Widget buttonActions() {
-  //   return new Row(
-  //     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-  //     children: <Widget>[
-  //       FlatButton.icon(
-  //         color: Colors.blueAccent,
-  //         icon: Icon(Icons.add),
-  //         label: Text(''),
-  //         onPressed: () => print('click!'),
-  //       ),
-  //       FlatButton.icon(
-  //         color: Colors.red,
-  //         icon: Icon(Icons.search),
-  //         label: Text(''),
-  //         onPressed: () => print('click!'),
-  //       )
-  //     ],
-  //   );
-  // }
+  Widget buttonActions() {
+    return new Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        FlatButton.icon(
+          color: Colors.blueAccent,
+          icon: Icon(Icons.send),
+          label: Text(''),
+          onPressed: () {
+            var currentForm = _formKey.currentState;
+
+            if(currentForm.validate()){
+              currentForm.save();
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ChatList()),
+              );
+            }
+          },
+        )
+      ],
+    );
+  }
 }
