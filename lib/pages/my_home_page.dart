a03043d92104dc97bc4d9351d06ff5b037b1d69e
Jamessom Queiroz <jamessomqueiroz@gmail.com>
2019-06-01 13:36:06 -0300

    wip home screen

--- lib/pages/my_home_page.dart
+++ lib/pages/my_home_page.dart
@@ -9,15 +9,82 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Container(),
+      resizeToAvoidBottomPadding: false,
+      // body: SafeArea(
+      //   child: Container(
+      //     margin: EdgeInsets.all(20.0),
+      //     child: Form(
+      //       key: _formKey,
+      //       child: Column(
+      //         children: [
+      //           Container(margin: EdgeInsets.only(top: 80.0),),
+      //           imageScreen(),
+      //           nameInput(),
+      //           // buttonActions(),
+      //         ],
+      //       ),
+      //     ),
+      //   ),
+      // ),
+      body: _body(),
     );
   }
+
+  _body() {
+    return Center(
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            //Container(margin: EdgeInsets.only(top: 80.0),),
+            imageScreen(),
+            nameInput(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget nameInput() {
+    return TextFormField(
+      decoration: InputDecoration(
+        hintText: 'Tony Stark',
+        labelText: 'Insert name'
+      ),
+      validator: (value) {
+        if (value.isEmpty) {
+          return 'Name canot be empty';
+        }
+      },
+    );
+  }
+
+  Widget imageScreen(){
+    return Image.asset('images/flutter.png');
+  }
+
+  // Widget buttonActions() {
+  //   return new Row(
+  //     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+  //     children: <Widget>[
+  //       FlatButton.icon(
+  //         color: Colors.blueAccent,
+  //         icon: Icon(Icons.add),
+  //         label: Text(''),
+  //         onPressed: () => print('click!'),
+  //       ),
+  //       FlatButton.icon(
+  //         color: Colors.red,
+  //         icon: Icon(Icons.search),
+  //         label: Text(''),
+  //         onPressed: () => print('click!'),
+  //       )
+  //     ],
+  //   );
+  // }
 }
